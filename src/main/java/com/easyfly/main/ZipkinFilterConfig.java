caa62058d2ad32b885700d4d2e11b93f15f921c4
MaGL <qsmsf@sohu.com>
2018-07-16 17:13:21 +0800

    增加zipkin和brave支持

--- src/main/java/com/easyfly/main/ZipkinFilterConfig.java
+++ src/main/java/com/easyfly/main/ZipkinFilterConfig.java
@@ -1,5 +1,9 @@
 package com.easyfly.main;
 
+import com.fasterxml.jackson.databind.deser.impl.ExternalTypeHandler;
+import com.github.kristofa.brave.Brave;
+import com.github.kristofa.brave.Sampler;
+import com.github.kristofa.brave.SpanCollector;
 import com.szga.xinghuo.api.base.zipkin.ZipkinFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ZipkinFilterConfig {
+
     @Bean
     public FilterRegistrationBean chkFilterRegistration(){
         FilterRegistrationBean bean = new FilterRegistrationBean();
